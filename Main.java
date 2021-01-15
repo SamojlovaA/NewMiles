74c33b6c644716b4b93117759a20c5be9ec2084f
SamojlovaA <samojlovaanna@icloud.com>
2021-01-15 22:50:26 +0400

    update

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int mile = 20;
-        int bonus = price/mile;
-        int miles = (int) service.calculate(price);
+        int miles = (int) service.calculate(price:10_000);
         System.out.println(miles);
     }
 }
